e40ef0753934a3e471efc4391b5469b21a803ba5
Niru Maheswaranathan <nirum@stanford.edu>
2015-06-03 23:13:13 -0700

    added simple command line argument parser using the argparser library

--- main.hs
+++ main.hs
@@ -3,15 +3,31 @@
 module Main where
 
 import Heather
+import System.Console.ArgParser
 
--- Set your zipCode code here!
-zipCode :: String
-zipCode = "94305"
+{--- Set your zipCode code here!-}
+{-zipCode :: String-}
+{-zipCode = "94305"-}
+
+{-main :: IO ()-}
+{-main = do-}
+  {-result <- getForecast zipCode-}
+  {-case result of-}
+    {-Left ex -> putStrLn $ "Caught exception: " ++ show ex-}
+    {-Right val -> putStrLn $ "Weather for " ++ (getForecast val) ++ ":"-}
+      {-where getForecast (Forecast (City city _) _) = city-}
+
+data MyArg = MyArg String deriving (Eq, Show)
+myParser :: ParserSpec MyArg
+myParser = MyArg `parsedBy` reqPos "query"
+
+app :: MyArg -> IO ()
+app (MyArg z)
+  | z == "forecast" = print "asked for Forecast"
+  | z == "current" = print "asked for Current"
+  | otherwise = error "Invalid argument"
 
 main :: IO ()
 main = do
-  result <- getCity zipCode
-  case result of
-    Left ex -> putStrLn $ "Caught exception: " ++ show ex
-    Right val -> putStrLn $ "Weather for " ++ (getCity val) ++ ":"
-      where getCity (Forecast (City city _) _) = city
+  interface <- mkApp myParser
+  runApp interface app
