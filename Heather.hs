f2883995fe6119fc1178685c4717a508c8d10656
Niru Maheswaranathan <nirum@stanford.edu>
2015-06-04 19:06:39 -0700

    refactoring + split forecast and current weather functions

--- Heather.hs
+++ Heather.hs
@@ -5,26 +5,26 @@ import Data.Aeson
 import Data.Text
 import Control.Applicative
 import Control.Monad
-import qualified Data.ByteString.Lazy as B
 import Network.HTTP.Conduit (simpleHttp)
 import Data.Time.Clock (UTCTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 
-{-
- - Loads data using the OpenWeatherMap API (http://openweathermap.org/api)
--}
+-- the OpenWeatherMap API url
+url :: (Show a) => String -> a -> String
+url q z = "http://api.openweathermap.org/data/2.5/" ++ q ++ "?q=" ++ (show z) ++ "&units=imperial"
 
--- city data type
+-- city datatype
 data City = City { name :: String
                  , country :: String } deriving Show
 
+-- cities contain "name" and "country" fields in the JSON schema
 instance FromJSON City where
   parseJSON (Object o) = City <$>
     o .: "name" <*>
     o .: "country"
   parseJSON _ = mzero
 
--- sample of weather data
+-- weather datatype
 data Weather =
   Weather { temperature :: Float            -- degrees Farenheit
           , pressure :: Float               -- hPa
@@ -36,35 +36,38 @@ data Weather =
           , time :: UTCTime                 -- POSIX timestamp
           , icon :: Text } deriving (Show)
 
--- container for city + a list of weather data
-data Forecast = Forecast { city :: City, weather :: [Weather] } deriving Show
-
+-- parse the weather JSON schema
 instance FromJSON Weather where
   parseJSON (Object o) = Weather <$>
     (main >>= (.: "temp")) <*>
     (main >>= (.: "pressure")) <*>
     (main >>= (.: "humidity")) <*>
     (wind >>= (.: "speed")) <*>
-    (clouds >>= (.: "all")) <*>
+    (cloud >>= (.: "all")) <*>
     (desc >>= (.: "main")) <*>
     (desc >>= (.: "description")) <*>
     liftM parseTime (o .: "dt") <*>
     (desc >>= (.: "icon"))
       where main = (o .: "main")
             wind = (o .: "wind")
-            clouds = (o .: "clouds")
+            cloud = (o .: "clouds")
             desc = (!! 0) <$> (o .: "weather")
             parseTime = posixSecondsToUTCTime . fromIntegral :: Int -> UTCTime
 
+-- a Forecast is a container for city + a list of weather data
+data Forecast = Forecast { city :: City, weather :: [Weather] } deriving Show
+
+-- pare the full JSON response
 instance FromJSON Forecast where
   parseJSON (Object o) = do
-    city <- parseJSON =<< (o .: "city")
-    weather <- parseJSON =<< (o .: "list")
-    return $ Forecast city weather
+    c <- parseJSON =<< (o .: "city")
+    w <- parseJSON =<< (o .: "list")
+    return $ Forecast c w
   parseJSON _ = mzero
 
-url :: String -> String -> String
-url t z = "http://api.openweathermap.org/data/2.5/" ++ t ++ "?q=" ++ z ++ "&units=imperial"
+-- main functions for querying the API and parsing the JSON response
+getForecast :: (Show a) => a -> IO (Either String Forecast)
+getForecast z = eitherDecode <$> ((simpleHttp . (url "forecast")) z)
 
-getForecast z = eitherDecode <$> ((simpleHttp . (url "forecast")) z) :: IO (Either String Forecast)
-getCurrent z = eitherDecode <$> ((simpleHttp . (url "weather")) z) :: IO (Either String Weather)
+getCurrent :: (Show a) => a -> IO (Either String Weather)
+getCurrent z = eitherDecode <$> ((simpleHttp . (url "weather")) z)
